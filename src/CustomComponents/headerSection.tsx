1a3b938f43ae64be8fc1a75ed4cc0c2e850a3ac2
Joshua Forster <joshuaforster@Joshuas-MacBook-Air.local>
2024-07-17 18:25:32 +0100

    changes

--- src/CustomComponents/headerSection.tsx
+++ src/CustomComponents/headerSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface HeaderSectionProps {
   image: string;
@@ -6,12 +6,27 @@ interface HeaderSectionProps {
 }
 
 const HeaderSection: React.FC<HeaderSectionProps> = ({ image, title }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 100); // slight delay for smoother animation
+  }, []);
+
   return (
-    <div className="relative w-full h-96 ">
+    <div className="relative w-full h-96">
       <img src={image} alt={title} className="w-full h-96 object-cover" />
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="absolute inset-0 flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-white mb-8">{title}</h1>
+        <h1
+          className={`text-4xl font-bold text-white mb-8 transition-transform ${
+            isVisible ? 'translate-x-0' : 'translate-x-[100vw]'
+          }`}
+          style={{ transitionDuration: '3s' }} // Set your desired transition duration here
+        >
+          {title}
+        </h1>
       </div>
     </div>
   );
