826e0062ff27b9032668837deb57451d1966b9e2
Joshua Forster <joshuaforster@Joshuas-MacBook-Air.local>
2024-07-17 14:43:37 +0100

    changes

--- src/MainPages/Projects.tsx
+++ src/MainPages/Projects.tsx
@@ -40,6 +40,7 @@ const Projects: React.FC<ProjectsProps> = ({ limit }) => {
       }`}
     >
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <h1>Projects</h1>
         <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-12 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {displayedProjects.map((project) => (
             <div key={project.id} className="flex flex-col items-center">
