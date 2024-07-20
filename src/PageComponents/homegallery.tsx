d5d55d2b5c90b8b5291a36d783c0a3eb9d146fc0
Joshua Forster <joshuaforster@Joshuas-MacBook-Air.local>
2024-07-20 08:51:55 +0100

    changes

--- src/PageComponents/homegallery.tsx
+++ src/PageComponents/homegallery.tsx
@@ -3,7 +3,7 @@ import Button from '../CustomComponents/buttons';
 
 const selectedImages = [
   'images/projectVI/Bumblebee%20Cottage,%20Main%20Street,%20Burton%20Overy-6.jpg',
-  'images/projectVI/Bumblebee%20Cottage,%20Main%20Street,%20Burton%20Overy-5.jpg',
+  'images/project1/Side-Elevation-1024x683.jpg',
   'images/projectVI/Bumblebee%20Cottage,%20Main%20Street,%20Burton%20Overy-2.jpg',
 ];
 
@@ -27,7 +27,6 @@ export default function HomeGallery() {
 
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
-
   return (
     <div
       ref={sectionRef}
@@ -35,30 +34,39 @@ export default function HomeGallery() {
         isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
       }`}
     >
-      <div className="text-center">
-        <h2 className="text-3xl font-bold text-fontColour">Gallery</h2>
-        <p className="mt-4 text-lg text-fontColour">
-          Take a look at some of the projects we've completed. Click the button below to see our full gallery.
-        </p>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 px-4 mt-8">
-        {selectedImages.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`Gallery Image ${index + 1}`}
-            className="w-full h-64 object-cover rounded-lg shadow-md"
-          />
-        ))}
-      </div>
-      <div className="flex justify-center mt-8">
-        <Button
-          to="/gallery"
-          variant="primary"
-          aria-label="Gallery Button"
-        >
-          View more
-        </Button>
+      <div className="px-4 lg:px-8 mx-auto max-w-screen-xl">
+        <div className="text-center">
+          <h2 className="text-3xl font-bold text-fontColour">Gallery</h2>
+          <p className="mt-4 text-lg text-fontColour">
+            Take a look at some of the projects we've completed. Click the button below to see our full gallery.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-8">
+          {selectedImages.map((image, index) => (
+            <div
+              key={index}
+              className="relative w-full overflow-hidden border border-black dark:border-white"
+              style={{ height: '500px' }} // Set a fixed height for all images
+            >
+              <div className="w-full h-full group">
+                <img
+                  src={image}
+                  alt={`Gallery Image ${index + 1}`}
+                  className="w-full h-full object-cover transition-transform duration-300 ease-in-out group-hover:brightness-75"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-center mt-8">
+          <Button
+            to="/gallery"
+            variant="primary"
+            aria-label="Gallery Button"
+          >
+            View more
+          </Button>
+        </div>
       </div>
     </div>
   );
