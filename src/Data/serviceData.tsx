b3c2d1e30eb62728fa2a64eeab3c73452550f9cd
Joshua Forster <joshuaforster@Joshuas-MacBook-Air.local>
2024-07-26 14:16:49 +0100

    changes

--- src/Data/serviceData.tsx
+++ src/Data/serviceData.tsx
@@ -4,21 +4,21 @@ export const services = [
     title: 'PROJECT PLANNING', 
     description: 'We offer comprehensive project planning services, including property sourcing, local surveys, architectural design and building, planning application assistance, and financial planning for builds.', 
     content: `Many homeowners are finding that it’s more practical to renovate their existing property in order to create their dream home, rather than selling up and looking for it. Lambert & Wright specialise in property renovations in the East Midlands and have a host of experience making many homeowners' dreams a reality.\n\nIt often comes as a surprise to homeowners when they find out what it costs to renovate a property, as it may be a lot less than you think. If you are looking for a leader in providing home renovations to the East Midlands, then you have come to the right place. With over 30 years’ experience, a team of trusted and experienced East Midlands tradesmen, and a long list of thrilled customers, there really is no better company to work with on your property renovations in the East Midlands than with Lambert & Wright.\n\nTrust in the knowledge that:\n\n- Project Management: We have experienced project managers that will be with you through to completion.\n- Trusted Suppliers: We use exclusive and trusted suppliers/merchants for all aspects of your renovation and always recommend our customers to these suppliers.\n- Tradesmen: All tradesmen that we use are vetted, are highly experienced and recommended.\n- Financial services: We can introduce you to our external financial advisors who can help you with any mortgage advice and assist with applications.`,
-    image: '/images/ServicesData/projectPlanning.jpg',
+    image: '/images/ServicesData/projectPlanning.webp',
   },
   { 
     id: 2, 
     title: 'BUILDING & CONSTRUCTION', 
     description: 'Our building and construction services encompass project management, building and carpentry work, roofing, and plastering, ensuring your renovation is completed to the highest standards.', 
     content: `Our building and construction services encompass project management and all aspects of whatever your project entails. A complete and comprehensive service that ensures that all your wishes are completed to a standard of excellence on time and within budget.`,
-    image: '/images/ServicesData/Building and Construction.jpg',
+    image: '/images/ServicesData/Building and Construction.webp',
   },
   { 
     id: 3, 
     title: 'PLUMBING & ELECTRICAL', 
     description: 'Our plumbing and electrical services include electrical design and installation, kitchen and bathroom installations, hot and cold pipework alterations, above ground drainage systems, hot water system upgrades, heating system upgrades, and boiler installations.', 
     content: `Our plumbing and electrical services include electrical design and installation, kitchen and bathroom installations, hot and cold pipework alterations, above ground drainage systems, hot water system upgrades, heating system upgrades, and boiler installations.\n\nWe specialise in:\n- Electrical Design & Installation\n- Kitchen & Bathroom Installations\n- Hot/Cold Pipework Alterations/Upgrades\n- Above Ground Drainage Systems\n- Hot Water System Upgrades\n- Heating System Upgrades\n- Boiler Installations\n\nOur team of certified plumbers and electricians are experienced in handling all types of plumbing and electrical work, ensuring your home systems are safe, efficient, and up-to-date.`,
-    image: '/images/ServicesData/Heating and Plumbing.jpg',
+    image: '/images/ServicesData/Heating and Plumbing.webp',
   },
   { 
     id: 4, 
@@ -32,7 +32,7 @@ export const services = [
     title: 'PROPERTY EXTENSIONS', 
     description: 'Need more space in your home but don’t want to move? An extension could be the perfect solution. Often, it’s cheaper to extend than to relocate. Whether you’re after a new kitchen, an extra bedroom, or additional living space, our team will guide you through the process, ensuring all rules and regulations are adhered to, to create the space you need.', 
     content: `Are you in need of more space in your home but don’t want to move? Getting an extension could be the solution you are looking for. In today’s market, it can actually be cheaper to extend than it is to move. Whether it’s a new kitchen that you are after, an extra bedroom, or an additional living area, an extension is a great way to provide that added space to your property to support your changing needs or your growing family.\n\nThere are various rules and regulations to be aware of when considering an extension as these can determine what is needed in order to start your project. Lambert & Wright have a team of experts who will assist you during this stage in the process to ensure that you get exactly what you want whilst adhering to all rules and regulations.`,
-    image: '/images/ServicesData/PropertyExtensions.jpg',
+    image: '/images/ServicesData/PropertyExtensions.webp',
   },
   { 
     id: 6, 
@@ -40,8 +40,8 @@ export const services = [
     description: 'Looking for creative flair outside as well as inside? We deliver functional and beautiful outdoor spaces. Our team is experienced in both hard and soft landscaping, catering to a range of budgets and requirements, ensuring a high-quality exterior finish that adds value to your property.', 
     content: `Are you looking for the same creative flair outside as you are inside? We have a creative eye for delivering on an outdoor space that is both functional and beautiful.\n\nOur team is highly experienced with both hard and soft landscaping and can cater to a range of budgets and requirements. We provide a high-quality landscaping service throughout the East Midlands, creating an incredible exterior finish that will add value to your property.\n\nOur landscaping services include:\n\n6//// Garden Design\n- Hard Landscaping (Patios, Driveways, Pathways)\n- Soft Landscaping (Planting, Lawns, Garden Maintenance)\n- Water Features\n- Outdoor Lighting\n\nLet us transform your outdoor space into a beautiful and functional area that you can enjoy all year round.`,
     images:[ 
-      '/images/ServicesData/Landscaping1.png',
-      '/images/ServicesData/Landscaping2.jpg',
+      '/images/ServicesData/Landscaping1.webp',
+      '/images/ServicesData/Landscaping2.webp',
     ]
   },
   { 
@@ -69,6 +69,6 @@ export const services = [
     title: 'HEATING INSTALLATION & UPGRADES', 
     description: 'Efficient heating systems not only provide comfort but also enhance energy efficiency. Whether you’re looking for underfloor heating, a new boiler system, or improvements to your existing system, Lambert & Wright will help you achieve the best results for your home.', 
     content: `Efficient heating systems in your home not only provide the comfort that you desire but, when selected correctly, can create a more energy-efficient home. Whether you are looking for underfloor heating, a new boiler system, or just making improvements to your existing system, you can work with Lambert & Wright to get the best result for your home.\n\nOur heating services include:\n\n- Underfloor Heating Installation\n- Boiler System Installation & Upgrades\n- Radiator Installation & Upgrades\n- Thermostat Installation\n- Heating System Maintenance & Repair\n\nLet us help you create a warm and comfortable home with our expert heating installation and upgrade services.`,
-    image: '/images/ServicesData/heating.png',
+    image: '/images/ServicesData/heating.webp',
   },
 ];
