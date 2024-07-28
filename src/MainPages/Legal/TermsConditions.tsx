3fc867ea83dbc15850902baea42d2b049a3d0605
Joshua Forster <joshuaforster@Joshuas-MacBook-Air.local>
2024-07-28 12:18:30 +0100

    changes

--- src/MainPages/Legal/TermsConditions.tsx
+++ src/MainPages/Legal/TermsConditions.tsx
@@ -6,38 +6,93 @@ export default function TermsConditions() {
       <div className="max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 md:grid-cols-12">
         <h1 className="text-4xl font-extrabold text-gray-900 dark:text-white mb-4">Terms and Conditions</h1>
         <div className="text-gray-700 dark:text-gray-400 space-y-4">
-          <p>Welcome to AD Forster Window Cleaning. By using our services, you agree to comply with and be bound by the following terms and conditions.</p>
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">1. These terms</h2>
+          <p>These are the terms and conditions on which we supply services to you. 
+          Please read these terms carefully before you confirm your acceptance to our quotation. These terms tell you who we are, how we will provide services to you, how you and we may change or end the contract, what to do if there is a problem and other important information.</p>
 
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">1. Services</h2>
-          <p>We provide professional window cleaning services, including but not limited to residential and commercial window cleaning, frames and sills cleaning, conservatory cleaning, and more. The scope of services will be agreed upon at the time of booking.</p>
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">2. Information about us and how to contact us</h2>
+          <p>We are JLR Bespoke Properties Limited, a company registered in England and Wales. Our company registration number is 10701923 and our registered office is at 19 Warren Park Way, Enderby, Leicester, LE19 4SA. 
+          You can contact us using the contact details on the quotation attached to these terms and conditions.
+          If we have to contact you, we will do so by telephone or by writing to you at the email address or postal address you provided to us in the quotation. 
+          "Writing" includes emails. When we use the words "writing" or "written" in these terms, this includes emails.</p>
 
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">2. Booking and Appointments</h2>
-          <p>Bookings can be made via our website, phone, or email. We will confirm all appointments and provide details about the scheduled service. Please ensure that access to the property is available at the agreed time.</p>
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">3. Our contract with you</h2>
+          <p>Our acceptance of your project will take place when you return a signed copy of these terms and conditions together with a 10% non-refundable deposit of the quotation cost, at which point a contract will come into existence between you and us. 
+          If we are unable to proceed with your project at this time, we will inform you of this.</p>
 
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">3. Payment</h2>
-          <p>Payment is due upon completion of the service unless otherwise agreed. We accept various forms of payment, including credit cards, bank transfers, and cash. Invoices will be provided upon request.</p>
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">4. Your rights to make changes</h2>
+          <p>If you wish to make a change to the services provided for in the quotation, please contact us. We will let you know if the change is possible. If it is possible, we will let you know about any changes to the price of the services, their timing or anything else which would be necessary as a result of your requested change and ask you to confirm whether you wish to go ahead with the change.</p>
 
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">4. Cancellations and Rescheduling</h2>
-          <p>If you need to cancel or reschedule an appointment, please provide at least 24 hours' notice. Cancellations made with less than 24 hours' notice may be subject to a cancellation fee.</p>
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">5. Our rights to make changes</h2>
+          <p>Minor changes to the services. We may change the services: 
+          to reflect changes in relevant laws and regulatory requirements; or
+          to implement minor technical adjustments and improvements.
+          More significant changes to the services and these terms. If we are required to make significant changes to the services and/or these terms, we will notify you and you may then contact us to end the contract and receive a full refund for any services not yet performed before the changes take effect.</p>
+          <p>Additional Services: If you wish to add additional services upon commencement of your project, please contact us. We will let you know if this is possible. If it is possible, we will provide you with a separate written quotation setting out the additional works and costs.</p>
 
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">5. Satisfaction Guarantee</h2>
-          <p>We strive for complete customer satisfaction. If you are not satisfied with our service, please contact us within 24 hours of the service, and we will address your concerns promptly.</p>
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">6. Providing the services</h2>
+          <p>We will supply the services to you from the date agreed in writing until we have completed the services. The estimated completion date for the services is as told to you during the quotation process. This date is an estimate only and we will not be liable for any failure to complete the services by the agreed date.
+          We are not responsible for delays outside our control. If our performance of the services is affected by an event outside our control, then we will contact you as soon as possible to let you know and we will take steps to minimise the effect of the delay. Provided we do this we will not be liable for delays caused by the event, but if there is a risk of substantial delay you may contact us to end the contract and receive a refund for any services you have paid for but not received.</p>
+          <p>If you have asked us to provide the services to you at your property and you do not allow us access to your property as arranged (and you do not have a good reason for this) we may charge you additional costs incurred by us as a result. If, despite our reasonable efforts, we are unable to contact you or re-arrange access to your property we may end the contract.</p>
+          <p>We may need certain information from you so that we can provide the services to you. We will contact you to ask for this information. If you do not, within a reasonable time of our request, provide us with this information, or you provide us with incomplete or incorrect information, we may either end the contract or make an additional charge of a reasonable sum to compensate us for any extra work that is required as a result. We will not be responsible for providing the services late or not providing any part of them if this is caused by your failure to provide information within a reasonable time.</p>
+          <p>Suspension of Services: We may have to suspend the services to deal with technical problems or make minor technical changes; update the services to reflect changes in relevant laws and regulatory requirements; or make changes to the services as requested by you or notified by us to you.</p>
+          <p>We will contact you in advance to tell you we will be suspending the services, unless the problem is urgent or an emergency. You may contact us to end the contract if we suspend the services for a period of more than 3 months and we will refund any sums you have paid in advance for services not provided to you.</p>
+          <p>We may also suspend the services if you do not pay. If you do not pay us for the services when you are supposed to and you still do not make payment within 5 days of us reminding you that payment is due, we may suspend supply of services until you have paid us the outstanding amounts. We will contact you to tell you we are suspending supply of the services. As well as suspending the services, we can also charge you interest on your overdue payments.</p>
 
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">6. Liability</h2>
-          <p>While we take utmost care in delivering our services, we are not liable for any damage caused by pre-existing conditions or improper installation of windows, frames, or sills. Please inform us of any fragile or high-value items near the service area.</p>
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">7. Your rights to end the contract</h2>
+          <p>You can always end the contract before the services have been supplied and paid for. You may contact us at any time to end the contract for the services, but in some circumstances we may charge you additional costs for doing so, as described below.
+          If you are ending the contract for a reason set out at (a) to (e) below the contract will end immediately and we will refund you in full for any services which have not yet been provided or have not been properly provided, less any costs incurred by us in preparing services. The reasons are:
+          we have told you about a significant change to the services or these terms which you do not agree to;
+          we have told you about an error in the price or description of the services and you do not wish to proceed;
+          there is a risk the services may be significantly delayed because of events outside our control;
+          we suspend the services for technical reasons for a period of more than 3 months; or
+          you have a legal right to end the contract because we are in breach of this contract.</p>
+          <p>If you terminate the contract for any reason other than those set out above, the contract will end immediately but we may charge you reasonable compensation for the net costs we will incur as a result of your ending the contract.</p>
 
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">7. Privacy</h2>
-          <p>Your privacy is important to us. Please refer to our Privacy Policy for details on how we collect, use, and protect your personal information.</p>
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">8. Our rights to end the contract</h2>
+          <p>We may end the contract at any time by writing to you if:
+          you do not make any payment to us when it is due and you still do not make payment within 5 days of us reminding you that payment is due;
+          you do not, within a reasonable time provide us with information that has been requested and is necessary for us to provide the services;
+          you do not, give us access to your property to enable us to provide the services to you.</p>
+          <p>If we end the contract in the situations set out above we will refund any money you have paid in advance for services we have not provided but we may deduct or charge you reasonable compensation for the net costs we will incur as a result of you breaking the contract.
+          If we terminate the services for any other reason, we will let you know in writing at least 5 days in advance of our stopping the services and will refund any sums you have paid in advance for services which will not be provided.</p>
 
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">8. Changes to Terms</h2>
-          <p>We reserve the right to update or modify these terms and conditions at any time. Any changes will be posted on our website, and it is your responsibility to review them periodically.</p>
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">9. If there is a problem with the services</h2>
+          <p>How to tell us about problems. If you have any questions or complaints about the services, please contact us using the contact details on your quotation.
+          We offer the following goodwill guarantee which is in addition to your legal rights and does not affect them. In the unlikely event there is any defect with the services:
+          if remedying the defect is impossible or cannot be done within a reasonable time or without significant inconvenience to you, we will refund the price you have paid for the element of the services which is defective.
+          in all other circumstances we will use every effort to repair or fix the defect free of charge, without significant inconvenience to you, as soon as we reasonably can.
+          We shall not have any liability to you for services beyond that set out in this clause.</p>
 
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">9. Contact Us</h2>
-          <p>If you have any questions or concerns about these terms and conditions, please contact us at:</p>
-          <ul className="list-inside">
-            <li><strong>Phone:</strong> <a href="tel:07342367824" className="text-blue-600 dark:text-blue-400 hover:underline">07342 367824</a></li>
-            <li><strong>Email:</strong> <a href="mailto:davidforster990@gmail.com" className="text-blue-600 dark:text-blue-400 hover:underline">davidforster990@gmail.com</a></li>
-          </ul>
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">10. Price and payment</h2>
+          <p>The price of the services (which includes VAT) will be the price set out in the quotation. We take all reasonable care to ensure that the prices of services advised to you are correct, however we may need to amend prices if the price of any goods or services purchased from a third party are increased during the term of the contract.</p>
+          <p>We will pass on changes in the rate of VAT. If the rate of VAT changes between your quotation date and the date we provide the services, we will adjust the rate of VAT that you pay, unless you have already paid for the services in full before the change in the rate of VAT takes effect.</p>
+          <p>The payment schedule shall be as follows:
+          First payment – 20% non-refundable deposit of the quotation cost followed by stage payments which will be invoiced fortnightly until final completion of services.
+          Only on receipt of payment of final invoice will we hand over all relative certificates including: Operational Manuals, NIC Electrical Certificate, Gas Safe Certificate and Local Authority Building Control Certificate.</p>
+          <p>We can charge interest if you pay late. If you do not make any payment to us by the due date, we may charge interest to you on the overdue amount at the rate of 3% a year above the base lending rate of the Bank of England from time to time. This interest shall accrue on a daily basis from the due date until the date of actual payment of the overdue amount, whether before or after judgment. You must pay us interest together with any overdue amount.
+          If you think an invoice is wrong, please contact us promptly to let us know. You will not have to pay any interest until the dispute is resolved. Once the dispute is resolved, we will charge you interest on correctly invoiced sums from the original due date.</p>
+
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">11. Our responsibility for loss or damage suffered by you</h2>
+          <p>We are responsible to you for foreseeable loss and damage caused by us. If we fail to comply with these terms, we are responsible for loss or damage you suffer that is a foreseeable result of our breaking this contract or our failing to use reasonable care and skill. Loss or damage is foreseeable if either it is obvious that it will happen or if, at the time the contract was made, both we and you knew it might happen, for example, if you discussed it with us during the sales process. Our liability for any loss or damage is limited to repair goods or reperform the services, where this is not possible or we are not able to repair goods or reperform services within a reasonable time our liability shall be limited to refunding the cost of the defective goods or services.</p>
+          <p>We do not exclude or limit in any way our liability to you where it would be unlawful to do so. This includes liability for death or personal injury caused by our negligence or the negligence of our employees, agents or subcontractors; for fraud or fraudulent misrepresentation; for breach of your legal rights in relation to the services.
+          If we are providing services in your property, we will make good any damage to your property caused by us while doing so, except as specifically excluded in the quotation. However, we are not responsible for the cost of repairing any pre-existing faults or damage to your property that we discover while providing the services. Our liability in this respect is capped in accordance with our public liability insurance which is capped at £2,000,000.
+          We are not liable for business losses. We only supply the services for domestic and private use. If you use the services for any commercial, business or re-sale purpose we will have no liability to you for any loss of profit, loss of business, business interruption, or loss of business opportunity.</p>
+
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">12. How we may use your personal information</h2>
+          <p>We will use the personal information you provide to us to:
+          provide the services;
+          process your payment for such services; and
+          if you agreed to this during the quotation process, to inform you about similar products that we provide, but you may stop receiving these communications at any time by contacting us.
+          We may pass your personal information to credit reference agencies. Where we extend credit to you for the services, we may pass your personal information to credit reference agencies and they may keep a record of any search that they do.</p>
+
+          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">13. Other important terms</h2>
+          <p>We may transfer our rights and obligations under these terms to another organisation. We will contact you to let you know if we plan to do this. If you are unhappy with the transfer, you may contact us to end the contract within one week of us telling you about it and we will refund you any payments you have made in advance for services not provided.
+          You may only transfer your rights or your obligations under these terms to another person with our written consent.
+          This contract is between you and us. No other person shall have any rights to enforce any of its terms.
+          If any court or relevant authority decides that any part of these conditions are unlawful, the remaining paragraphs will remain in full force and effect.
+          Even if we delay in enforcing this contract, we can still enforce it later. If we do not insist immediately that you do anything you are required to do under these terms, or if we delay in taking steps against you in respect of your breaking this contract, that will not mean that you do not have to do those things or prevent us taking steps against you at a later date. For example, if you miss a payment and we do not chase you but we continue to provide the services, we can still require you to make the payment at a later date.
+          These terms are governed by English law and you can bring legal proceedings in respect of the services in the English courts.</p>
         </div>
       </div>
     </section>
